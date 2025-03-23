f687fe147fc10a7f9287c378c90a9ccf5ce9514f
Nicolae Postica <nicolae.postica2@gmail.com>
2025-03-23 02:09:14 +0200

    update comon lib

--- src/sl/mouse.py
+++ src/sl/mouse.py
@@ -1,48 +1,48 @@
-import pyautogui
-from time import sleep
+# import pyautogui
+# from time import sleep
 
-pyautogui.MINIMUM_DURATION = 0
-pyautogui.MINIMUM_SLEEP = 0
-pyautogui.PAUSE = 0
-pyautogui.FAILSAFE = False
+# pyautogui.MINIMUM_DURATION = 0
+# pyautogui.MINIMUM_SLEEP = 0
+# pyautogui.PAUSE = 0
+# pyautogui.FAILSAFE = False
 
 
-class MouseControls:
-    type = "pyautogui"
+# class MouseControls:
+#     type = "pyautogui"
 
-    @staticmethod
-    def move(args):
-        x, y = args
-        pyautogui.moveTo(x, y)
+#     @staticmethod
+#     def move(args):
+#         x, y = args
+#         pyautogui.moveTo(x, y)
 
-    @staticmethod
-    def move_click(args, wait=0):
-        x, y = args
-        pyautogui.moveTo(x, y)
-        pyautogui.click()
-        sleep(wait)
+#     @staticmethod
+#     def move_click(args, wait=0):
+#         x, y = args
+#         pyautogui.moveTo(x, y)
+#         pyautogui.click()
+#         sleep(wait)
 
-    @staticmethod
-    def move_relative(rel_x, rel_y):
-        pyautogui.moveRel(rel_x, rel_y)
+#     @staticmethod
+#     def move_relative(rel_x, rel_y):
+#         pyautogui.moveRel(rel_x, rel_y)
 
-    @staticmethod
-    def click(wait=0):
-        pyautogui.leftClick()
-        sleep(wait)
+#     @staticmethod
+#     def click(wait=0):
+#         pyautogui.leftClick()
+#         sleep(wait)
 
-    @staticmethod
-    def get_position():
-        point = pyautogui.position()
-        return point.x, point.y
+#     @staticmethod
+#     def get_position():
+#         point = pyautogui.position()
+#         return point.x, point.y
 
-    @staticmethod
-    def tab(number):
-        sleep(2)
-        for _ in range(number):
-            pyautogui.press('tab')
-            sleep(1)
+#     @staticmethod
+#     def tab(number):
+#         sleep(2)
+#         for _ in range(number):
+#             pyautogui.press('tab')
+#             sleep(1)
 
-    @staticmethod
-    def enter():
-        pyautogui.press("enter")
+#     @staticmethod
+#     def enter():
+#         pyautogui.press("enter")
